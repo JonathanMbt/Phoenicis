94bf9fcb1d0edf8f076b2bdcf4dc2d07198cb1cb
Jonathan Mabit <jonathan.mabit@dametis.com>
2022-09-20 12:09:49 +0200

    feat(wip): splashscreen

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,7 +1,31 @@
-import { FC } from 'react'
+import { FC } from 'react';
 
 const App: FC = () => {
-  return <>HELLO</>
-}
+  return (
+    <SplashScreen>
+      <Stack
+        alignSelf="center"
+        flexDirection="column"
+        justifyContent="center"
+        flexGrow={0.5}
+      >
+        <Typography variant="h1">Forgotten Shores</Typography>
+      </Stack>
+      <Stack
+        alignSelf="center"
+        flexDirection="column"
+        justifyContent="center"
+        flexGrow={1}
+      >
+        <Typography variant="subtitle1">lorem ipsum</Typography>
+      </Stack>
+      <Stack
+        flexDirection="column"
+        justifyContent="center"
+        flexGrow={0.5}
+      ></Stack>
+    </SplashScreen>
+  );
+};
 
-export default App
+export default App;
