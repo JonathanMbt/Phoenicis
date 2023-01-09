b3bdf47acececd9e667e264adffab02e50c0b8e9
Jonathan Mabit <jonathan.mabit@dametis.com>
2023-01-09 12:53:37 +0100

    chore(www): refactor img url & add package for icons

--- www/src/components/Footer.tsx
+++ www/src/components/Footer.tsx
@@ -2,74 +2,52 @@ import { Button, ButtonGroup, IconButton, Stack, Typography } from '@mui/materia
 import { FC, PropsWithChildren, useCallback } from 'react';
 import BetterButton from './UI/BetterButton/BetterButton';
 import i18next from 'i18next';
-import logo from 'www/public/images/logoPhoenicisGame.png';
-import { grey } from '@mui/material/colors';
 import { useTranslation } from 'react-i18next';
 
-
 const Footer: FC = () => {
+  const { t } = useTranslation('footer');
 
-    const { t } = useTranslation('footer');
+  const changeLng = useCallback(async (language: string) => {
+    await i18next.changeLanguage(language);
+  }, []);
 
-    const changeLng = useCallback(async (language: string) => {
-        await i18next.changeLanguage(language);
-    }, []);
+  return (
+    <Stack
+      flexDirection="row"
+      height="15vh"
+      minHeight="100px"
+      justifyContent={'space-between'}
+      sx={{ backgroundColor: '#D4CBBA' }}
+      position="absolute"
+      left="0"
+      right="0"
+      bottom="0"
+      px={2}
+    >
+      <Stack flexDirection="column" justifyContent={'center'}>
+        <Stack flexDirection="row">
+          <img src="/images/logoPhoenicisGame.png" alt="Image" height={30} width={30} />
+          <Typography variant="body2">{t('title.corp')}</Typography>
+        </Stack>
+        <a href="/documents/productys.pdf" download>
+          <Typography variant="body2">{t('title.agreements')}</Typography>
+        </a>
+      </Stack>
 
-    return (
-        <Stack
-            flexDirection="row"
-            height="15vh"
-            minHeight='100px'
-            justifyContent={'space-between'}
-            sx={{ backgroundColor: '#D4CBBA' }}
-            position='absolute'
-            left='0'
-            right='0'
-            bottom='0'
-            px={2}
-        >
-            <Stack
-                flexDirection="column"
-                justifyContent={'center'}
-            >
-                <Stack flexDirection="row">
-                    <img src={logo} alt="Image" height={30} width={30} />
-                    <Typography variant='body2'>{t('title.corp')}</Typography>
-                </Stack>
-                <a href='/documents/productys.pdf' download><Typography variant='body2'>{t('title.agreements')}</Typography></a>
-            </Stack>
+      <Stack flexDirection="column" justifyContent={'center'}>
+        <Typography variant="body2">{t('title.network')}</Typography>
+        <Stack flexDirection="row" justifyContent={'center'}></Stack>
+      </Stack>
 
-            <Stack
-                flexDirection="column"
-                justifyContent={'center'}
-            >
-                <Typography variant='body2'>{t('title.network')}</Typography>
-                <Stack
-                    flexDirection="row"
-                    justifyContent={'center'}
-                >
-                </Stack>
-                <Stack
-                    flexDirection="row"
-                    justifyContent={'center'}
-                >
-
-                </Stack>
-            </Stack>
-
-            <Stack
-                flexDirection="column"
-                justifyContent={'center'}
-            >
-                <ButtonGroup size="small" color="inherit" variant="text" orientation='vertical'>
-                    <BetterButton onClick={() => changeLng('fr')}>FR</BetterButton>
-                    <BetterButton onClick={() => changeLng('en')}>EN</BetterButton>
-                    <BetterButton onClick={() => changeLng('es')}>ES</BetterButton>
-                </ButtonGroup>
-            </Stack>
-
-        </Stack >
-    );
+      <Stack flexDirection="column" justifyContent={'center'}>
+        <ButtonGroup size="small" color="inherit" variant="text" orientation="vertical">
+          <BetterButton onClick={() => changeLng('fr')}>FR</BetterButton>
+          <BetterButton onClick={() => changeLng('en')}>EN</BetterButton>
+          <BetterButton onClick={() => changeLng('es')}>ES</BetterButton>
+        </ButtonGroup>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default Footer;
